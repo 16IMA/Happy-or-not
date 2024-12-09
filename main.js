01a0c96a6670530a653572d52b10e866900f97af
16IMA <imonfort001@gmail.com>
2024-12-09 20:02:37 +0100

    start js code

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+//Establecemos variables con los elementos del HTMl a usar
+
+const faceEmoticon = document.querySelector(".js_faceEmoticon");
+const selectorFace = document.querySelector(".js_selectorFace");
+const body = document.querySelector("body");
+const selectionBtn = document.querySelector(".js_selectionBtn");
+
+
+
+selectionBtn.addEventListener("click", (ev) => {
+    debugger;
+    ev.preventDefault();
+    const newFace = selectorFace.value;
+    if(newFace ==="happy"){
+        faceEmoticon.innerHTML = ":)"
+    }
+    else {
+    faceEmoticon.innerHTML = ":("
+    };
+})
+
+
+
